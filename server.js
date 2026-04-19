a1e6b90cf7fa907526f1d0f4405ad18cfc6155df
Yaman Kumar Yadav <143440408+YamanYadav01@users.noreply.github.com>
2026-04-19 21:00:12 +0530

    Enable CORS middleware in server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(cors({
   credentials: true
 }));
 
-// app.use(cors()); 
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); // enable cookie support
